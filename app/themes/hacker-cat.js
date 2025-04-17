8e7bca0106e052db3f512da2de5aa76645517655
Liam Greene <liamgreene90@gmail.com>
2025-04-17 18:34:16 -0400

    Update hacker-cat.js

--- app/themes/hacker-cat.js
+++ app/themes/hacker-cat.js
@@ -6,5 +6,5 @@ export default {
   errorOutputColor: '#ffa1a1',
   promptSymbolColor: '#20C90E',
   fontSize: '16px',
-  url: "url('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmt6YzBia2FjZ3F4YnZxeTZ2d3l0aDZyamE4dTVlYnZua3c2cDViNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/24hRt4e4FOX6PxVgGy/giphy.gif')"
+  url: "url('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmt6YzBia2FjZ3F4YnZxeTZ2d3l0aDZyamE4dTVlYnZua3c2cDViNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/24hRt4e4FOX6PxVgGy/giphy.gif')",
 };
