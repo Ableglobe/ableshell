43e28ffe1ced062f92988d5d93746288bd9a4730
Liam Greene <liamgreene90@gmail.com>
2025-04-17 16:40:13 -0400

    Update filesystem.js

--- app/terminal/filesystem.js
+++ app/terminal/filesystem.js
@@ -1,118 +1,78 @@
 import { FileSystem } from 'javascript-terminal-turtle';
-const _0x174321 = {
+
+// File contents
+const experienceContent = {
   content: "Security Innovation - Remote - Pentester\n-> August 2023 - Present\n\nSecurity Innovation - Seattle, WA - Security Engineer Intern\n-> May 2023 - August 2023\n\nPurdue University - West Lafayette, IN - Windows System Administration TA\n-> August 2021 - May 2023\n\nDonaldson - Minneapolis, MN - SOC Intern\n-> June 2022 - August 2022"
 };
-const _0xbc4b5c = {
+
+const aboutContent = {
   content: "Hi, I'm Liam.\n\nI started my education at the University of Washington in Seattle. In 2020, I transferred to Purdue University \nto pursue Cybersecurity in the Dept. of Computer & Information Technology, where I earned my bachelors.\n\nI currently work as a penetration tester for Security Innovation. I primarily work on web applications, but my\ninterests in the field include AD, network, IoT, and hardware hacking."
 };
-const _0x8e3c9c = {
+
+const personalContent = {
   content: "HackTheBox - Online Cybersecurity CTF\n-> Peaked top 10 in the US, top 80 globally (100+ solved machines)\n\nHackTheBox - Pro Labs\n-> Cybernetics (28 machines, Red Team Operator L2)\n-> Offshore (21 machines, Pentester L3)\n-> Zephyr (17 machines, Red Team Operator L1)\n-> Dante (14 machines, Pentester L2)\n\nVulnLab - Cybersecurity Labs\n-> Completed standalone machines, AD chains, and Wutai Lab\n\nNational Cyber League Spring 2022 CTF\n-> 1st in Web App Exploitation, 17th overall\n\nTracerFire 2022 CTF - Purdue University\n-> 1st Place"
 };
-const _0x1cf51b = {
-  "content": "root:x:0:0:root:/root:/usr/bin/bash\nliam:x:1000:1000:liam:/home/liam:/bin/bash\nlawrence:x:1001:1001:lawrence:/home/lawrence:/bin/bash\nted:x:1002:1002:ted:/home/ted:/bin/bash\njenny:x:1003:1003:jenny:/home/jenny:/bin/bash"
+
+const passwdContent = {
+  content: "root:x:0:0:root:/root:/usr/bin/bash\nliam:x:1000:1000:liam:/home/liam:/bin/bash\nlawrence:x:1001:1001:lawrence:/home/lawrence:/bin/bash\nted:x:1002:1002:ted:/home/ted:/bin/bash\njenny:x:1003:1003:jenny:/home/jenny:/bin/bash"
 };
-const _0x5d15b6 = {
+
+const hostnameContent = {
   content: "vghs"
 };
-const _0x5b3a79 = {
+
+const hostsContent = {
   content: "127.0.0.1\tlocalhost\n127.0.1.1\tvghs"
 };
-const _0x6aeeec = {
-  content: ''
-};
-const _0x13a4af = {
-  content: ''
-};
-const _0x169001 = {
-  content: ''
-};
-const _0x3b40e6 = {
-  content: ''
-};
-const _0x1996fa = {
-  content: ''
-};
-const _0x5aa831 = {
-  content: ''
-};
-const _0x3f7b54 = {
-  content: ''
-};
-const _0x4f6f86 = {
-  content: ''
-};
-const _0x6718a7 = {
-  content: ''
-};
-const _0x2f5343 = {
-  content: ''
-};
-const _0x2f41e3 = {
-  content: ''
-};
-const _0x12eeb5 = {
-  content: ''
-};
-const _0x273231 = {
-  content: ''
-};
-const _0xabfc1d = {
-  content: ''
-};
-const _0x39b7c7 = {
-  content: ''
-};
-const _0x298fab = {
-  content: ''
-};
-const _0x45545c = {
-  content: ''
-};
-const _0x5aec08 = {
-  content: ''
-};
-const _0x283b77 = {
-  content: ''
-};
-const _0x363dd6 = {
+
+const emptyContent = {
   content: ''
 };
 
-const _0x55851c = {
+// Directory structure
+const fileSystemStructure = {
+  // Home directory
   "/home": {},
   "/home/liam": {},
-  "/home/liam/about.txt": _0xbc4b5c,
-  "/home/liam/personal.txt": _0x8e3c9c,
-  "/home/liam/experience.txt": _0x174321,
+  "/home/liam/about.txt": aboutContent,
+  "/home/liam/personal.txt": personalContent,
+  "/home/liam/experience.txt": experienceContent,
   "/home/lawrence": {},
   "/home/ted": {},
   "/home/jenny": {},
+  
+  // System directories
   "/etc": {},
-  "/etc/passwd": _0x1cf51b,
-  "/etc/hostname": _0x5d15b6,
-  "/etc/hosts": _0x5b3a79,
+  "/etc/passwd": passwdContent,
+  "/etc/hostname": hostnameContent,
+  "/etc/hosts": hostsContent,
+  
+  // Binaries
   "/bin": {},
-  "/bin/bash": _0x6aeeec,
-  "/bin/sh": _0x13a4af,
-  "/bin/cp": _0x3b40e6,
-  "/bin/cd": _0x1996fa,
-  "/bin/mkdir": _0x5aa831,
-  "/bin/rmdir": _0x3f7b54,
-  "/bin/ls": _0x4f6f86,
-  "/bin/cat": _0x6718a7,
-  "/bin/clear": _0x2f5343,
-  "/bin/echo": _0x2f41e3,
-  "/bin/head": _0x12eeb5,
-  "/bin/history": _0x273231,
-  "/bin/index": _0xabfc1d,
-  "/bin/whoami": _0x39b7c7,
-  "/bin/touch": _0x298fab,
-  "/bin/tail": _0x45545c,
-  "/bin/rm": _0x5aec08,
-  "/bin/printenv": _0x283b77,
+  "/bin/bash": emptyContent,
+  "/bin/sh": emptyContent,
+  "/bin/cp": emptyContent,
+  "/bin/cd": emptyContent,
+  "/bin/mkdir": emptyContent,
+  "/bin/rmdir": emptyContent,
+  "/bin/ls": emptyContent,
+  "/bin/cat": emptyContent,
+  "/bin/clear": emptyContent,
+  "/bin/echo": emptyContent,
+  "/bin/head": emptyContent,
+  "/bin/history": emptyContent,
+  "/bin/index": emptyContent,
+  "/bin/whoami": emptyContent,
+  "/bin/touch": emptyContent,
+  "/bin/tail": emptyContent,
+  "/bin/rm": emptyContent,
+  "/bin/printenv": emptyContent,
+  
+  // Other directories
   "/var": {},
   "/vghs": {}
 };
 
-const _0x1b4b12 = FileSystem.create(_0x55851c);
-export default _0x1b4b12;
+// Create and export the filesystem
+const fileSystem = FileSystem.create(fileSystemStructure);
+export default fileSystem;
